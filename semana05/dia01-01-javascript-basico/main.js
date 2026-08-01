fac2b286c086b82ed42a2743f40cc47a831af169
GContreras <gcontrerasx@gmail.com>
2026-07-31 19:23:07 -0600

    pagina mi portafolio semana 4

---
+++ semana05/dia01-01-javascript-basico/main.js
@@ -0,0 +1,55 @@
+//1. COMENTARIOS
+console.log('Hola Javascript')
+
+// Comentarios, Javascript
+
+/*
+Esto es un comentario
+de
+varias 
+lineas 
+ */
+
+//2. TIPOS DE DATOS
+//Primitivos basicos:
+//2.1 Number (numeros)
+//Ejemplos: 123, 34.89, -67, 0
+
+console.log(20);
+console.log(typeof 20);
+console.log(123);
+console.log(typeof 123);
+console.log(34.89, typeof(34.89));
+console.log(-67,typeof(-67));
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
+//2.2 String (Texto)
+
+console.log("German Contreras", typeof("German Contreras"))
+
+//2.3 Boolean (verdadero o falso)
+
+console.log(true,false,typeof(true))
+
+
+//2.4 undefined, no tiene valor aun
+let x
+console.log(x)
+
+//2.5 null, intencionalmente está vacío
+let nombre = null;
+console.log(nombre);
+
+//Verificar que tipos de datos devuelven las siguientes lineas
+
+
+console.log('-------------------------------------------')
+
+console.log(typeof 10);
+console.log(typeof "hola");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
