9ccd5a4e3624e1488d3721b5201d382953aa7194
GContreras <gcontrerasx@gmail.com>
2026-08-12 18:58:23 -0600

    cambios en main de programacion intermedia javascript y ejercicios

--- semana05/dia02-01-javascript-intermedio/main.js
+++ semana05/dia02-01-javascript-intermedio/main.js
@@ -9,9 +9,9 @@
 
 //Declaracion
 
-const arregloVacio=[]
+const arregloVacio = []
 const listaDeNumeros = [25, 36, 89, -99, 15.60]
-const listaDeValores = [1,2,3, 'German', 'contreras', 'codigo', true, null, undefined]
+const listaDeValores = [1, 2, 3, 'German', 'contreras', 'codigo', true, null, undefined]
 
 console.log(arregloVacio);
 console.log(listaDeNumeros);
@@ -25,8 +25,8 @@ console.log(listaDeValores[15])//undefined
 
 //Escritura en un arreglo
 
-listaDeValores[3]='gcj';
-Object.freeze(listaDeValores);    
+listaDeValores[3] = 'gcj';
+Object.freeze(listaDeValores);
 
 console.log(listaDeValores[3]);
 console.log(listaDeValores);
@@ -37,7 +37,7 @@ listaDeValores.push('Final');
 console.log(listaDeValores);
 */
 
-const listaDeNombre = ['Daril','Jhoel'];
+const listaDeNombre = ['Daril', 'Jhoel'];
 console.log(listaDeNombre);
 listaDeNombre.push('Francisco');
 console.log(listaDeNombre);
@@ -47,14 +47,14 @@ listaDeNombre.pop();
 console.log(listaDeNombre);
 
 //Insertar un elemento en una posicion determinada
-listaDeNombre.splice(0,0,'Lucy')
+listaDeNombre.splice(0, 0, 'Lucy')
 console.log(listaDeNombre);
 
-listaDeNombre.splice(2,0,'Amanda')
+listaDeNombre.splice(2, 0, 'Amanda')
 console.log(listaDeNombre);
 
 //Eliminar un elmento en una posicion determinada
-listaDeNombre.splice(2,1);
+listaDeNombre.splice(2, 1);
 console.log(listaDeNombre);
 
 //Obtener el tamaño de nuestro arreglo
@@ -62,27 +62,27 @@ console.log(listaDeNombre.length);// 3
 console.log('bienvenido'.length);// 10
 
 //Obtener el ultimo elemento del arreglo
-console.log(listaDeNombre[listaDeNombre.length-1]);
+console.log(listaDeNombre[listaDeNombre.length - 1]);
 console.log(listaDeNombre.at(0));
 console.log(listaDeNombre.at(-1));// Jhoel, el ultimo valor
 console.log(listaDeNombre.at(-2));
 
 //To Do: investigar funcion slice
 const frutas = ["manzana", "durazno", "mango", "pera", "uvas", "banano"];
-console.log(frutas.slice(2,4));
-console.log(listaDeValores.slice(0,3));
+console.log(frutas.slice(2, 4));
+console.log(listaDeValores.slice(0, 3));
 
 
 //Metodos de arreglos (includes, filter, map, sort, foreach, reduce)
 
 //Metodo Includes, nos indica si el valor que se pasa como parametro esta en el arreglo devuelve true o false
 
-const lenguajes = ['javascript', 'php','python', 'C', 'c++','java','python']
+const lenguajes = ['javascript', 'php', 'python', 'C', 'c++', 'java', 'python']
 console.log(lenguajes.includes('java'));
 console.log(lenguajes.includes('cobol'));
 
 //Metodo Filter, nos ayuda a ubicar un elemento dentro de un arreglo usando una condicion y devuelve un nuevo arreglo
-const resultado = lenguajes.filter(function(item) {
+const resultado = lenguajes.filter(function (item) {
     //return item === 'python'
     //return item.includes('c') || item.includes('C')
     return item.toLocaleLowerCase().includes('c')
@@ -93,7 +93,7 @@ console.log(resultado)
 //Metodo MAP, evalua un arreglo, lo modifica y hay que pasarle una funcion.
 console.log(lenguajes);
 
-cont = nombreConTituloEspecial = lenguajes.map(function(lenguaje) {
+const nombreConTituloEspecial = lenguajes.map(function (lenguaje) {
     //logica para evaluar y modificar el arreglo
     //return 'hola'
     return '*' + lenguaje + '*'
@@ -113,14 +113,14 @@ console.log(lenguajes);
 
 //Metodo foreach, nos ayuda a recorrer un arreglo
 
-for (let index =0; index<10; index++){
+for (let index = 0; index < 10; index++) {
     console.log(index)
 }
 
 console.log('-------------------------------')
 const miArreglo = [];
 
-lenguajes.forEach(function(lenguaje) {
+lenguajes.forEach(function (lenguaje) {
     console.log('#', lenguaje)
     miArreglo.push('El mejor; ' + lenguaje)
 })
@@ -130,55 +130,55 @@ console.log(miArreglo);
 //metodo reduce, nos ayuda a acumular sumar, los valores de un arreglo
 
 // con for
-numeros = [3,40,102,7,50]
+numeros = [3, 40, 102, 7, 50]
 
 let sumatoria = 0;
- for (let i=0; i <numeros.length; i++) {
-    sumatoria = sumatoria +numeros[i]
- }
- console.log(sumatoria);
+for (let i = 0; i < numeros.length; i++) {
+    sumatoria = sumatoria + numeros[i]
+}
+console.log(sumatoria);
 
- const sumatoriaConReduce = numeros.reduce(function(acumulador, valorActual) {
-    return acumulador+valorActual
- })
- console.log(sumatoriaConReduce);
+const sumatoriaConReduce = numeros.reduce(function (acumulador, valorActual) {
+    return acumulador + valorActual
+}, 0)
+console.log(sumatoriaConReduce);
 
- //Objetos,  una forma de guardar informacion en pares de clave: valor
- //Un objeto  sirve para agregar datos relacionados en un solo lugar
- /*
- {
- key: value,
- key2: value2,
- key3: value3
- }
- */
+//Objetos,  una forma de guardar informacion en pares de clave: valor
+//Un objeto  sirve para agregar datos relacionados en un solo lugar
+/*
+{
+key: value,
+key2: value2,
+key3: value3
+}
+*/
 
- const miObjetoVacio = {}
+const miObjetoVacio = {}
 
- console.log(miObjetoVacio);
+console.log(miObjetoVacio);
 
- const persona = {
+const persona = {
     nombre: 'German C.',
     edad: 46,
     esProgramador: true,
     'mi color favorito': 'azul',
-    coloresFavoritos : ['rosado', 'rojo', 'verde']
- }
+    coloresFavoritos: ['rosado', 'rojo', 'verde']
+}
 
- const persona2 = {
+const persona2 = {
     nombre: 'Francisco',
     edad: 66,
     esProgramador: false
- }
+}
 
- //Leer los campos de un objeto (notacion de punto y de corchete)
- console.log(persona.nombre) // German C.
- console.log(persona.edad)//28
- console.log(persona2.edad)//66
- console.log(persona.estadoCivil)//undefined
- console.log(persona.coloresFavoritos)
-console.log(persona.coloresFavoritos[0]);
-console.log(persona['mi color favorito']);
+//Leer los campos de un objeto (notacion de punto y de corchete)
+console.log(persona.nombre) // German C.
+console.log(persona.edad)//28
+console.log(persona2.edad)//66
+console.log(persona.estadoCivil)//undefined
+console.log(persona.coloresFavoritos) //['rosado', 'rojo', 'verde']
+console.log(persona.coloresFavoritos[0]);// rosado
+console.log(persona['mi color favorito']); //azul
 
 
 //Eliminar las propiedades de un objeto
@@ -189,7 +189,18 @@ persona2.esProgramador = undefined;
 
 //Insertar una nueva propiedad a un objeto
 
-persona.platilloFavorito = 'Ceviche de conchas negras'
-persona['juegos favoritos'] = ['Crash Team Racing', 'Mario', 'Minecraft']
+persona.platilloFavorito = 'Ceviche de conchas negras';
+persona['juegos favoritos'] = ['Crash Team Racing', 'Mario', 'Minecraft'];
 
-console.log(persona)
+console.log(persona);
+
+
+const fruits = ["apple", "banana", "orange"];
+const result = fruits.filter(fruit => fruit);
+console.log(result);
+
+let text = "12345";
+let min = Math.min(text);
+let max = Math.max(...text);
+console.log(min);
+console.log(max);
