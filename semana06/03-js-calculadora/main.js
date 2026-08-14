87e10ef349d8106ab746a5de358bffadbfed72d2
GContreras <gcontrerasx@gmail.com>
2026-08-14 14:30:21 -0600

    se agrego archivo css para calculadora, en js se agrego logica de aritmetica y en html clases para el css

--- semana06/03-js-calculadora/main.js
+++ semana06/03-js-calculadora/main.js
@@ -3,40 +3,51 @@ let operador = ''
 let operando = ''
 
 //Consultar un elemento a la vez
-const  inputDisplay = document.querySelector('#inputDisplay')
-//console.log()({inputDisplay})
+const inputDisplay = document.querySelector('#inputDisplay')
+//console.log({inputDisplay})
+//console.log(inputDisplay);
 
 //consultar multiples elementos a la vez
 const buttons = document.querySelectorAll('.button')
 //console.log(buttons)
 
 //Eventos
-buttons.forEach(function(button){
-    button.addEventListener('click', function(event) {
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (event) {
         //console.log('hice click!', event.target)
 
         const buttonText = event.target.textContent
 
         console.log(buttonText)
 
-        if('*-+'.includes(buttonText)) {
+        if ('*-+'.includes(buttonText)) {
             //02. accionamos el operador
             operador = buttonText
             operando = Number(numeroActual)
             numeroActual = '0'
-        } else if (buttonText === '='){
+        } else if (buttonText === '=') {
             // calcular las operaciones en base al numero actual y el operando
+            if (operador === '+') {
+                numeroActual =  operando +  numeroActual
+            }
+            else if (operador === '-') {
+                numeroActual = operando - numeroActual
 
+            } else if (operador === '*') {
+                numeroActual = operando * numeroActual
+            }
 
-        } else {
-            //01 se presiono algun numero
-            numeroActual= Number(numeroActual + buttonText)
+        } else if (buttonText === 'CE') {
+            //Limpiar el operando, operador y el input
+            numeroActual = '0'
+            operador = ''
+            operando = ''
         }
-    inputDisplay.value= numeroActual
 
-        //01 se presiono algun numero
-        numeroActual = Number(numeroActual+ buttonText)
-
-        //inputDisplay.value = numeroActual
+        else {
+            //01 se presiono algun numero
+            numeroActual = Number(numeroActual + buttonText)
+        }
+        inputDisplay.value = numeroActual
     })
 })
