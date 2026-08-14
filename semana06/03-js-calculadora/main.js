13abb2b50b310685d8b4d15f3c0db877d2bda1a4
gcontreras <gcontrerasx@gmail.com>
2026-08-13 22:05:28 -0600

    dom en html

---
+++ semana06/03-js-calculadora/main.js
@@ -0,0 +1,42 @@
+let numeroActual = '0'
+let operador = ''
+let operando = ''
+
+//Consultar un elemento a la vez
+const  inputDisplay = document.querySelector('#inputDisplay')
+//console.log()({inputDisplay})
+
+//consultar multiples elementos a la vez
+const buttons = document.querySelectorAll('.button')
+//console.log(buttons)
+
+//Eventos
+buttons.forEach(function(button){
+    button.addEventListener('click', function(event) {
+        //console.log('hice click!', event.target)
+
+        const buttonText = event.target.textContent
+
+        console.log(buttonText)
+
+        if('*-+'.includes(buttonText)) {
+            //02. accionamos el operador
+            operador = buttonText
+            operando = Number(numeroActual)
+            numeroActual = '0'
+        } else if (buttonText === '='){
+            // calcular las operaciones en base al numero actual y el operando
+
+
+        } else {
+            //01 se presiono algun numero
+            numeroActual= Number(numeroActual + buttonText)
+        }
+    inputDisplay.value= numeroActual
+
+        //01 se presiono algun numero
+        numeroActual = Number(numeroActual+ buttonText)
+
+        //inputDisplay.value = numeroActual
+    })
+})
