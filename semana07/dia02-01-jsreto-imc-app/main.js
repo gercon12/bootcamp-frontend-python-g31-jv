1b48e3e54e3253b57d733cc6134e04c745d846ee
gcontreras <gcontrerasx@gmail.com>
2026-08-23 17:12:52 -0600

    actualizacion proyecto IMC colores de respuesta imc

---
+++ semana07/dia02-01-jsreto-imc-app/main.js
@@ -0,0 +1,60 @@
+// **Condiciones del IMC:**
+
+// * Baja: < 18.5
+// * Normal: < 18.5 - 24.9
+// * Sobrepeso: < 25 - 29.9 
+// * Obeso: > 30
+
+const imcPeso = document.querySelector('.imc__peso')
+const imcAltura = document.querySelector('.imc__altura')
+const imcButton = document.querySelector('.imc__button')
+const imcResultado = document.querySelector('.imc__resultado')
+
+
+imcButton.addEventListener('click', (event) => {
+    console.log('calculando imc')
+    let resultadoImc = 0
+    const peso = Number(imcPeso.value)
+    const altura = Number(imcAltura.value) / 100
+
+
+    if (peso === 0 || altura === 0 || typeof (peso) === 'string' || typeof (altura) === 'string') {
+        imcResultado.textContent = 'invalido'
+        imcResultado.classList.add('text-white')
+        return
+    } 
+
+
+    resultadoImc = peso / ((altura ** 2)).toFixed(1)
+    console.log('Su IMC es: ', resultadoImc)
+
+
+    imcResultado.classList.remove(
+        'text-white',
+        'text-yellow-500',
+        'text-green-500',
+        'text-orange-500',
+        'text-red-500'
+    )
+
+
+    if (resultadoImc < 18.5) {
+        imcResultado.textContent = ' Baja'
+        imcResultado.classList.add('text-yellow-500')
+    } else if (resultadoImc < 25) {
+        imcResultado.textContent = ' Normal'
+        imcResultado.classList.add('text-green-500')
+    } else if (resultadoImc < 30) {
+        imcResultado.textContent = ' Sobrepeso'
+        imcResultado.classList.add('text-orange-500')
+    } else {
+        imcResultado.textContent = 'Obeso'
+        imcResultado.classList.add('text-red-500')
+    }
+    //imcResultado.textContent = resultadoImc
+
+}
+)
+
+
+
