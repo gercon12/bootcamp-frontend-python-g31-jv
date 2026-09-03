ebb5072c0e37ed4df2a79bb6ee070468e8ac21f7
GContreras <gcontrerasx@gmail.com>
2026-09-02 19:20:39 -0600

    edicion, eliminar personajes y retoques en html header, mensaje de datos invalidos

--- semana08/dia02-02-js-fetch-dragon-ball-crud/main.js
+++ semana08/dia02-02-js-fetch-dragon-ball-crud/main.js
@@ -22,6 +22,35 @@ const cargarPersonajes = async () => {
 
 //----------------------Renderizar Personajes--------------------------
 
+// Funcion para estilo de Genero
+function generoFunction(personaje) {
+  const genero = personaje.genero.toLowerCase()
+  let generoClass = ''
+
+  if (genero === 'male') {
+    generoClass = 'text-green-600 border border-green-600 px-1.5 py-0.5 rounded bg-green-50 hover:bg-green-200'
+  } else if (genero === 'female') {
+    generoClass = 'text-pink-600 border border-pink-600 px-1.5 py-0.5 rounded bg-pink-50 hover:bg-pink-200'
+  }
+  return generoClass
+}
+
+//Funcion para estilo de raza
+function razaFunction(personaje) {
+  const raza = personaje.raza.toLowerCase()
+  let razaClass = ''
+
+  if (raza === 'saiyan') {
+    razaClass = 'text-blue-600 border border-blue-600 px-1.5 py-0.5 rounded bg-blue-50 hover:bg-blue-200'
+  } else if (raza === 'human') {
+    razaClass = 'text-orange-600 border border-orange-600 px-1.5 py-0.5 rounded bg-orange-50 hover:bg-orange-200'
+  } else if (raza === 'namekian') {
+    razaClass = 'text-purple-600 border border-purple-600 px-1.5 py-0.5 rounded bg-purple-50 hover:bg-purple-200'
+  }
+  return razaClass
+}
+
+//Renderizar personajes
 const renderPersonajes = (personajes = []) => {
 
   // Seleccionar la lista donde se mostrarán los personajes
@@ -40,47 +69,30 @@ const renderPersonajes = (personajes = []) => {
     // Crear elemento <li>
     const li = document.createElement('li')
 
+    // Validar el género del personaje y asignar la clase correspondiente
+    const generoClass = generoFunction(personaje)
+
+    // Validar la raza del personaje y asignar la clase correspondiente
+    const razaClass = razaFunction(personaje)
+
     // Agregar estilos al elemento
-    li.className = 'flex items-center gap-4 bg-white border border-neutral-200 rounded-xl px-4 py-3 hover:border-neutral-300 transition-colors'
-
-   
-//Validar el género del personaje y asignar la clase correspondiente
-    const gender =  personaje.gender.toLowerCase()
-    let genderClass = ''
-    
-    if (gender === 'male') { 
-      genderClass = 'text-green-600 border border-green-600 px-1.5 py-0.5 rounded bg-green-50'
-  } else if (gender === 'female') {
-    genderClass = 'text-pink-600 border border-pink-600 px-1.5 py-0.5 rounded bg-pink-50'
-  }
-
-  //Validar la raza del personaje y asignar la clase correspondiente
-  const race = personaje.race.toLowerCase()
-  let raceClass = ''
-
-  if (race === 'saiyan') {
-    raceClass = 'text-blue-600 border border-blue-600 px-1.5 py-0.5 rounded bg-blue-50'
-  } else if (race === 'human') {
-    raceClass = 'text-orange-600 border border-orange-600 px-1.5 py-0.5 rounded bg-orange-50'
-  } else if (race === 'namekian') {
-    raceClass = 'text-purple-600 border border-purple-600 px-1.5 py-0.5 rounded bg-purple-50'
-  }
+    li.className = 'flex items-center gap-4 bg-white border border-neutral-200 rounded-xl px-4 py-3 hover:border-neutral-300 transition-colors mb-3'
 
 
-   // Crear el contenido del personaje
-  li.innerHTML = `
+    // Crear el contenido del personaje
+    li.innerHTML = `
 
 
       <div class="shrink-0 w-22 h-22 overflow-hidden rounded-lg border border-neutral-200 flex items-center justify-center bg-neutral-50">
-        <span class="font-mono text-base font-medium">${personaje.name}</span>
-        <img src="${personaje.image}" alt="${personaje.name}" class="w-full h-full object-cover object-[50%_5%]">
+        <span class="font-mono text-base font-medium"></span>
+        <img src="${personaje.imagen}" alt="${personaje.nombre}" class="w-full h-full object-cover object-[50%_5%]">
       </div>
 
       <div class="flex-1 min-w-0">
-        <p class="text-sm font-bold truncate">${personaje.name}</p>
+        <p class="text-sm font-bold truncate">${personaje.nombre}</p>
         <div class="flex items-center gap-2 mt-1">
-          <span class="${raceClass} text-[16px] font-mono px-1.5 py-0.5 rounded">${personaje.race}</span>
-          <span class="${genderClass} text-[16px] font-mono px-1.5 py-0.5 rounded">${personaje.gender}</span>
+          <span class="${razaClass} text-[16px] font-mono px-1.5 py-0.5 rounded">${personaje.raza}</span>
+          <span class="${generoClass} text-[16px] font-mono px-1.5 py-0.5 rounded">${personaje.genero}</span>
         </div>
       </div>
 
@@ -93,8 +105,179 @@ const renderPersonajes = (personajes = []) => {
         </button>
       </div>
     `
-  lista.appendChild(li)
-})
+    lista.appendChild(li)
+  })
 }
+
+//----------------------Crear el Personaje--------------------------
+const form = document.querySelector("#form")
+
+form.addEventListener('submit', async (event) => {
+  //Evitar que el formulario recargue la pagina
+  event.preventDefault()
+
+  //limpiar contenido de p 'invalido'
+  document.querySelector('#nombreError').textContent = ''
+  document.querySelector('#imagenError').textContent = ''
+
+  console.log('Guardando personaje')
+
+  //Obtener el formulario
+  const personajeForm = document.forms['form']
+
+  //Obtener datos ingresador por el usuario
+  const nombre = personajeForm.nombre.value
+  const imagen = personajeForm.imagen.value
+  const raza = personajeForm.raza.value
+  const genero = personajeForm.genero.value
+
+  console.log(nombre, imagen, raza, genero)
+
+
+  if (nombre.trim() === '') {
+    document.querySelector('#nombreError').textContent = 'Nombre inválido'
+    return
+  } else if (imagen.trim() === '') {
+    document.querySelector('#imagenError').textContent = 'URL inválida'
+    return
+  }
+
+
+  //crear objeto con datos del personaje
+  const nuevoPersonaje = {
+    nombre,
+    imagen,
+    raza,
+    genero
+  }
+
+  //Configurar peticion POST
+  const opciones = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(nuevoPersonaje)
+  }
+
+  try {
+    //Enviar el corredor a la API
+    const response = await fetch(API_URL, opciones)
+
+    //Verificar si la peticion fue exitosa
+    if (!response.ok) {
+      throw new Error('Tuvimos problemas al cargar el personaje')
+    }
+    console.log('Personaje se guardo correctamente')
+
+    // Actualizar la lista
+    cargarPersonajes()
+
+    // Limpiar formulario
+    personajeForm.reset()
+
+  } catch (error) {
+
+    // Mostrar error en consola
+    console.log(error)
+  }
+
+})
+
+//----------------------Editar o Eliminar Personajes--------------------------
+
+lista.addEventListener('click', async (event) => {
+  //Verificar que boton se ha presionado
+  console.log({ target: event.target })
+
+
+  // Verificar que se haya presionado un botón
+  if (event.target.tagName === 'BUTTON') {
+
+    // Obtener acción e ID del botón
+    const { action, id } = event.target.dataset
+
+    //Eliminar personaje
+
+    if (action === 'eliminar') {
+      //Pedir confirmacion eliminar
+      const confirmado = confirm('Desea eliminar personaje, Acción no se puede deshacer')
+
+      if (!confirmado) {
+        return
+      }
+
+      //Configurar opciones DELETE
+      opciones = {
+        method: 'DELETE',
+      }
+
+      //Eliminar personaje usando su ID
+      const response = await fetch(`${API_URL}/${id}`, opciones)
+
+      //Verificar respuesta
+      if (!response.ok) {
+        console.log('No se pudo eliminar al personaje')
+      }
+      //Actualizar lista
+      cargarPersonajes()
+    }
+
+    //Editar personaje
+    if (action === 'editar') {
+
+      //Pedir confirmacion
+      const confirmado = confirm('Desea editar personaje')
+
+      if (!confirmado) {
+        return
+      }
+      //Obtener datos del formulario
+      const personajeForm = document.forms['form']
+
+      const nombre = personajeForm.nombre.value
+      const imagen = personajeForm.imagen.value
+      const raza = personajeForm.raza.value
+      const genero = personajeForm.genero.value
+
+      //Crear objeto con los datos actualizados
+      const actualizadoPersonaje = {
+        nombre,
+        imagen,
+        raza,
+        genero
+      }
+
+      //Configurar peticion PUT
+      const opciones = {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(actualizadoPersonaje)
+      }
+
+      //Actualizar personaje con su ID
+      const response = await fetch(`${API_URL}/${id}`, opciones)
+
+      //Verificar respuesta
+      if (!response) {
+        console.log('No se pudo editar personaje')
+        return
+      }
+
+      console.log('Personaje actualizado correctamente')
+
+      //Actualizar lista
+      cargarPersonajes()
+
+      //Limpiar el formulario
+      personajeForm.reset()
+
+    }
+  }
+
+})
+
 
 cargarPersonajes()
