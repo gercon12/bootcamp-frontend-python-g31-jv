cb102111eabd3854c94a256f71735d1b351ad427
gcontreras <gcontrerasx@gmail.com>
2026-09-11 22:09:50 -0600

    agregar clear

--- semana10/dia01-02-react-student-list-crud/src/App-sin-storage.jsx
+++ semana10/dia01-02-react-student-list-crud/src/App-sin-storage.jsx
@@ -156,7 +156,7 @@ return (
         />
       </label>
 
-      <div className="flex gap-4">
+      <div>
         <input
           className="bg-blue-700 text-white hover:bg-blue-800 font-medium
           rounded-lg text-sm w-full px-4 py-2 text-center cursor-pointer"
