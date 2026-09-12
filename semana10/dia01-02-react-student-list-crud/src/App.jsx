a855847d8c569b9db6d0226db9d2bd09a62c077f
gcontreras <gcontrerasx@gmail.com>
2026-09-11 21:39:50 -0600

    editar y eliminar

--- semana10/dia01-02-react-student-list-crud/src/App.jsx
+++ semana10/dia01-02-react-student-list-crud/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Swal from 'sweetalert2'
 
 const App = () => {
   const DEFAULT_STUDENTS = [
@@ -32,6 +33,30 @@ const App = () => {
     event.preventDefault()
     console.log('Guardando....')
 
+
+    //Actualizar un estudiante
+    if (form.id) {
+      const updateStudents = students.map(student => {
+        if (student.id === form.id) {
+          return {
+            ...student,
+            name: form.name,
+            city: form.city
+          }
+        }
+        return student
+      })
+      setStudents(updateStudents)
+      setForm({
+        id: '',
+        name: '',
+        city: ''
+      })
+      return
+    }
+
+
+    //creando un nuevo estudiante
     const newStudent = {
       id: crypto.randomUUID(),
       name: form.name,
@@ -48,6 +73,7 @@ const App = () => {
 
   }
 
+
   const handleChange = (event) => {
     const { name, value } = event.target
     setForm({ ...form, [name]: value })
@@ -55,96 +81,132 @@ const App = () => {
 
 
 
-  return (
-    <main className="w-96 mx-auto border border-slate-400 rounded-lg mt-6 p-4">
-      <h1 className="text-2xl text-center text-slate-700 font-bold mb-4"> Student CRUD</h1>
+  const handleDelete = (id) => {
+    console.log('Eliminando', id)
 
-      <form
-        className="flex flex-col gap-4 bg-slate-100 p-3 rounded-lg border"
-        onSubmit={handleSave}
-      >
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const updatedStudents = students.filter(student => {
+          return student.id !== id
+        })
+         setStudents(updatedStudents)
+    }
+  });   
+}
 
-        <label className="flex flex-col gap-2">
-          <span className="text-sm font-medium text-slate-900">Name</span>
-          <input
-            className="bg-slate-50 border border-slate-300 text-slate-90 text-sm
+  const handleEdit = (student) => {
+    console.log('Actualizando', student)
+    setForm({
+      id: student.id,
+      name: student.name,
+      city: student.city
+    })
+  }
+
+  const handleClear = () => {
+    setForm({
+      id: '',
+      name: '',
+      city: ''
+    })
+  }
+
+return (
+  <main className="w-96 mx-auto border border-slate-400 rounded-lg mt-6 p-4">
+    <h1 className="text-2xl text-center text-slate-700 font-bold mb-4"> Student CRUD</h1>
+
+    <form
+      className="flex flex-col gap-4 bg-slate-100 p-3 rounded-lg border"
+      onSubmit={handleSave}
+    >
+
+      <label className="flex flex-col gap-2">
+        <span className="text-sm font-medium text-slate-900">Name</span>
+        <input
+          className="bg-slate-50 border border-slate-300 text-slate-90 text-sm
           rounded-lg w-full px-4 py-2"
-            type="text"
-            name="name"
-            placeholder="Ej. German Contreras"
-            required
-            onChange={handleChange}
-            value={form.name}
-          />
-        </label>
+          type="text"
+          name="name"
+          placeholder="Ej. German Contreras"
+          required
+          onChange={handleChange}
+          value={form.name}
+        />
+      </label>
 
-        <label>
-          <span>City</span>
-          <input
-            className="bg-slate-50 border border-slate-300 text-slate-90 text-sm
+      <label>
+        <span>City</span>
+        <input
+          className="bg-slate-50 border border-slate-300 text-slate-90 text-sm
           rounded-lg w-full px-4 py-2"
-            type="text"
-            name="name"
-            placeholder="Ej. El Progreso"
-            required
-            onChange={handleChange}
-            value={form.city}
-          />
-        </label>
+          type="text"
+          name="city"
+          placeholder="Ej. El Progreso"
+          required
+          onChange={handleChange}
+          value={form.city}
+        />
+      </label>
 
-        <div>
-          <input
-            className="bg-blue-700 text-white hover:bg-blue-800 font-medium
+      <div>
+        <input
+          className="bg-blue-700 text-white hover:bg-blue-800 font-medium
           rounded-lg text-sm w-full px-4 py-2 text-center cursor-pointer"
-            type="submit"
-            value="Save"
-          />
+          type="submit"
+          value="Save"
+        />
 
-          <input
-            className="bg-slate-500 text-white hover:bg-blue-800 font-medium
+        <input
+          className="bg-slate-500 text-white hover:bg-blue-800 font-medium
           rounded-lg text-sm w-full px-4 py-2 text-center cursor-pointer"
-            type="reset"
-            value="Clear" />
+          type="reset"
+          value="Clear" />
 
-        </div>
+      </div>
+    </form>
 
-        <h2 className="text-center text-slate-700 font-bold my-4">Student List</h2>
+    <h2 className="text-center text-slate-700 font-bold my-4">Student List</h2>
 
-        <section className="mt-4 flex flex-col gap-2">
-          <div className="flex justify-between items-center gap-2 bg-slate-300 px-4 py-2
+    <section className="mt-4 flex flex-col gap-2">
+      <div className="flex justify-between items-center gap-2 bg-slate-300 px-4 py-2
           rounded-lg">
-            <div className="text-left">Name</div>
-            <div className="text-left">city</div>
-            <div className="text-left gap-2">Actions</div>
+        <div className="text-left">Name</div>
+        <div className="text-left">city</div>
+        <div className="text-left gap-2">Actions</div>
+      </div>
+
+
+      {students.map(student => {
+        return (
+
+          <div className="flex justify-between items-center gap-2 bg-slate-200 px-4 py-2
+          rounded-lg">
+            <div className="text-left">{student.name}</div>
+            <div className="text-left">{student.city}</div>
+            <div className="flex gap-2">
+              <button onClick={() => handleEdit(student)}>✏</button>
+              <button onClick={() => handleDelete(student.id)}>❌</button>
+            </div>
           </div>
+        )
+      })}
 
 
-          {students.map(student => {
-            return (
+      <pre>{JSON.stringify(form, null, 2)}</pre>
+      <pre>{JSON.stringify(students, null, 2)}</pre>
 
-              <div className="flex justify-between items-center gap-2 bg-slate-200 px-4 py-2
-          rounded-lg">
-                <div className="text-left">{student.name}</div>
-                <div className="text-left">{student.city}</div>
-                <div className="flex gap-2">
-                  <button>✏</button>
-                  <button>❌</button>
-                </div>
-              </div>
-            )
-          })}
+    </section>
 
-
-          <pre>{JSON.stringify(form, null, 2)}</pre>
-          <pre>{JSON.stringify(students, null, 2)}</pre>
-
-        </section>
-
-
-
-      </form>
-    </main>
-  )
+  </main>
+)
 
 }
 
